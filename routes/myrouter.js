777371e0c83ba73f78ff173fcb35c90e317c667e
AKASUsan <kasubaranime@gmail.com>
2026-01-10 11:53:28 +0700

    add id to file json

--- routes/myrouter.js
+++ routes/myrouter.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const path = require("path");
-const path_product = path.join(__dirname, "../views/products/products.json");
+const pdata = path.join(__dirname, "../views/products/products.json");
+const prouter = path.join(__dirname, "./myrouter.js");
+
 const products = [
   {
     name: "โน๊ตบุ๊ค",
@@ -23,6 +25,36 @@ const products = [
     description: "Earphones",
   },
 ];
+
+function read() {
+  try {
+    const rawData = fs.readFileSync(pdata, "utf8");
+    return JSON.parse(rawData);
+  } catch (error) {
+    return [];
+  }
+}
+function readroute() {
+  let data = fs.readFileSync(pdata, "utf8");
+  let dataArray = JSON.parse(data);
+
+  dataArray.push(products);
+
+  const updatedJson = JSON.stringify(dataArray, null, 2);
+  fs.writeFileSync(pdata, updatedJson, "utf8");
+}
+
+function write(data) {
+  fs.writeFileSync(pdata, JSON.stringify(data, null, 2), "utf8");
+}
+
+// function writeproduct(data) {
+//   fs.writeFile(prouter, JSON.stringify(data, null, 4), "utf8", (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+//   });
+// }
+
 router.get("/", (req, res) => {
   res.render("index.ejs", {
     products: products,
@@ -47,84 +79,37 @@ router.get("/manage", (req, res) => {
     }
   );
 });
+
 //TODO : GET to file json
-// router.get("/insert", (req, res) => {
-//   // fs.readFile(
-//   //   path.join(__dirname, "../views/products/products.json"),
-//   //   "utf-8",
-//   //   (err, data) => {
-//   //     if (err) {
-//   //       console.error(err);
-//   //       return res.status(500).json({ error: "Failed to read data file" });
-//   //     }
-//   //     res.status(200).json(JSON.parse(data));
-//   //   }
-//   // );
-// });
+router.get("/insert", (req, res) => {
+  const items = read();
+  res.json(items);
+
+  res.render("index.ejs");
+  //
+});
+
 //TODO: POST to use json
 router.post("/insert", (req, res) => {
+  const items = read();
+
   const newItem = {
-    id: Date.now(),
+    id: req.body.id,
     name: req.body.name,
     price: req.body.price,
     image: req.body.image,
     description: req.body.description,
   };
 
-  if (!newItem) {
-    return res.status(400).json({ error: "Request body cannot be empty" });
-  }
-  fs.readFile(path_product, "utf-8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: "Error reading data file" });
-    }
-    let items = [];
-    if (data) {
-      try {
-        items = JSON.parse(data);
-      } catch (parseError) {
-        console.error(parseError);
-        return res.status(500).json({ error: "Error parsing JSON data" });
-      }
-    }
-    items.push(newItem);
-    fs.writeFile(path_product, JSON.stringify(items, null, 2), (err) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ error: "Error writing data to file" });
-      }
-      console.log("New item added and saved to file.");
-      res
-        .status(201)
-        .json({ message: "Item added successfully", item: newItem });
-    });
-    file = "../routes/myrouter.js";
-    fs.readFile(file, "utf8", (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      try {
-        const jsonObject = JSON.parse(data);
-        jsonObject.push(products);
-        fs.writeFile(
-          file,
-          JSON.stringify(jsonObject, null, 2),
-          "utf8",
-          (err) => {
-            if (err) {
-              console.error(err);
-            } else {
-              console.log("Data successfully added to file.");
-            }
-          }
-        );
-      } catch (parseError) {
-        console.error("Error parsing JSON:", parseError);
-      }
-    });
-  });
+  const nextId = items.length > 0 ? items[items.length - 1].id + 1 : 1;
+
+  newItem.id = nextId;
+
+  items.push(newItem);
+  items.push(products);
+  write(items);
+  readroute();
+
   res.render("index.ejs", {
     products: products,
   });
